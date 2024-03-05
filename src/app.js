e603ba92f47f422e2aab0b96313ff8e9fcab0524
Santiago Galardi <santygalardi@gmail.com>
2024-03-05 20:43:43 -0300

    new variables

--- src/app.js
+++ src/app.js
@@ -6,27 +6,27 @@ const routes = require('./routes/index.js');
 
 require('./db.js');
 
-const server = express();
+const app = express();
 
-server.name = 'API';
+app.name = 'API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', 'https://pi-videogames-client-rho.vercel.app');
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-client-rho.vercel.app');
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-server.use('/', routes);
+app.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
@@ -34,4 +34,4 @@ server.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
-module.exports = server;
+module.exports = app;

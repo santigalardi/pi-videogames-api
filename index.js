0f73442e3bb44d01d3427bbdbed80ebf6bad3be8
Santiago Galardi <santygalardi@gmail.com>
2024-03-05 19:44:56 -0300

    env changes

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ const { PORT } = process.env;
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at', PORT); // eslint-disable-line no-console
+    console.log('%s listening at', PORT);
   });
 });
--- src/app.js
+++ src/app.js
@@ -15,7 +15,8 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-videogames-client-rho.vercel.app');
+  // res.header('Access-Control-Allow-Origin', 'https://pi-videogames-client-rho.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
